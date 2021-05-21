0a8e80442e65e1172afdecac0bab2a84c087c16a
Ibrahim778 <m.ibrahim0798@gmail.com>
2021-05-21 14:33:19 +0500

    Debugger Redesign

--- src/debug.c
+++ src/debug.c
@@ -5,11 +5,22 @@
 #include <vita2d.h>
 #include "common.h"
 
+#define StartY 20
+#define TextColour RGBA8(0,255,0,255)
+#define SelectColour RGBA8(0,247,255,255)
+#define MaxSelections 2
+
+void DebugScreen();
+void TriggerSwap();
+void SelectScreen();
 extern SceUID _vshKernelSearchModuleByName(const char *name, SceUInt64 *unk);
 
 vita2d_pgf *pgf;
 
 int moduleLoaded = 0;
+
+char Selections[MaxSelections][400] = { "Debug Bluetooth", "Swap triggers and bumpers" };
+void (*functions[MaxSelections])(void) = { &DebugScreen, &TriggerSwap };
 
 void drawBuff()
 {   
@@ -43,44 +54,40 @@ void drawBuff()
     GetPidVid(&pid, &vid);
 
     sprintf(outBuff, "Last connection attempt had PID: 0x%X VID: 0x%X\n", pid, vid);
-    vita2d_pgf_draw_text(pgf, 0, 45, RGBA8(0,247,255,255), 1.0f,outBuff);
-    if(swapStatus) vita2d_pgf_draw_text(pgf, 0, 65, RGBA8(0,247,255,255), 1.0f, "Triggers and bumpers are swapped");
-    else vita2d_pgf_draw_text(pgf, 0, 65, RGBA8(0,247,255,255), 1.0f, "Triggers and bumpers are not swapped");
+    vita2d_pgf_draw_text(pgf, 0, 45, SelectColour, 1.0f,outBuff);
+    if(swapStatus) vita2d_pgf_draw_text(pgf, 0, 65, SelectColour, 1.0f, "Triggers and bumpers are swapped");
+    else vita2d_pgf_draw_text(pgf, 0, 65, SelectColour, 1.0f, "Triggers and bumpers are not swapped");
+    vita2d_pgf_draw_text(pgf, 0, 85, TextColour, 1.0f, "Hold Select + Start + L + R to exit");
 }
 
-int main()
+void TriggerSwap()
 {
-    sceShellUtilInitEvents(0);
-    sceShellUtilLock(SCE_SHELL_UTIL_LOCK_TYPE_PS_BTN_2);
+    vita2d_start_drawing();
+    vita2d_clear_screen();
 
-    SceUInt64 searchBuff = 0;
-    moduleLoaded = (_vshKernelSearchModuleByName("X1Vita", &searchBuff) >= 0);
-    SceCtrlData buttons; 
-    if(moduleLoaded)
+    if(GetSwapStatus())
     {
-        for (int i = 0; i < 10; i++)
-        {
-            sceCtrlReadBufferPositive(0, &buttons, 1);
-            if(buttons.buttons & SCE_CTRL_CROSS)
-            {
-                int toSet = 1;
-                SetSwapStatus(&toSet);
-                break;
-            }
-            if(buttons.buttons & SCE_CTRL_CIRCLE)
-            {
-                int toSet = 0;
-                SetSwapStatus(&toSet);
-                break;
-            }
-        }
+        vita2d_pgf_draw_text(pgf, 0, StartY, TextColour, 1.0f, "Triggers are no longer swapped");
+        int swap = 0;
+        SetSwapStatus(&swap);
+    }
+    else
+    {
+        vita2d_pgf_draw_text(pgf, 0, StartY, TextColour, 1.0f, "Triggers are now swapped");
+        int swap = 1;
+        SetSwapStatus(&swap);
     }
 
-    vita2d_init();
-    vita2d_set_clear_color(RGBA8(0x40, 0x40, 0x40, 0xFF));
-    
-    pgf = vita2d_load_default_pgf();
+    vita2d_end_drawing();
+    vita2d_swap_buffers();
+    sceShellUtilUnlock(SCE_SHELL_UTIL_LOCK_TYPE_PS_BTN_2);
+    sceKernelDelayThread(3 * 1000000);
+    sceShellUtilLock(SCE_SHELL_UTIL_LOCK_TYPE_PS_BTN_2);
+}
 
+void DebugScreen()
+{
+    SceCtrlData buttons;
     do
     {
         sceCtrlPeekBufferPositive(0, &buttons, 1);
@@ -90,9 +97,78 @@ int main()
         vita2d_end_drawing();
         vita2d_swap_buffers();
     } while (((buttons.buttons & SCE_CTRL_START) && (buttons.buttons & SCE_CTRL_SELECT) && (buttons.buttons & SCE_CTRL_LTRIGGER) && (buttons.buttons & SCE_CTRL_RTRIGGER)) == 0);
+    SelectScreen();
+    sceKernelDelayThread(20000);
+}
+
+void PrintSelection(int selection)
+{
+    int printY = StartY;
+    vita2d_pgf_draw_text(pgf, 0, printY, TextColour, 1.0f, "Thank you for using X1Vita! Press Start to exit");
+    printY += StartY + 10;
+    for (int i = 0; i < MaxSelections; i++)
+    {
+        if(selection == i) vita2d_pgf_draw_text(pgf, 0, printY, SelectColour, 1.0f, Selections[i]);
+        else vita2d_pgf_draw_text(pgf, 0, printY, TextColour, 1.0f, Selections[i]);
+        printY += StartY;
+    }
+}
+
+void SelectScreen()
+{
+    SceCtrlData pad;
+    int CurrentSelection = 0;
+    do
+    {
+        sceCtrlPeekBufferPositive(0, &pad, 1);
+        vita2d_start_drawing();
+        vita2d_clear_screen();
+        PrintSelection(CurrentSelection);
+        vita2d_end_drawing();
+        vita2d_swap_buffers();
+
+        if(pad.buttons & SCE_CTRL_UP)
+        {
+            if(CurrentSelection > 0) 
+            {
+                CurrentSelection--;
+                sceKernelDelayThread(200000);
+            }
+        }
+        if(pad.buttons & SCE_CTRL_DOWN)
+        {
+            if(CurrentSelection < MaxSelections - 1) 
+            {
+                CurrentSelection++;
+                sceKernelDelayThread(200000);
+            }
+        }
+        if(pad.buttons & SCE_CTRL_CROSS)
+        {
+           functions[CurrentSelection]();
+           sceKernelDelayThread(200000);
+        }
+    } while (!(pad.buttons & SCE_CTRL_START));
+    
+}
+
+int main()
+{
+    sceShellUtilInitEvents(0);
+    sceShellUtilLock(SCE_SHELL_UTIL_LOCK_TYPE_PS_BTN_2);
+
+    SceUInt64 searchBuff = 0;
+    moduleLoaded = (_vshKernelSearchModuleByName("X1Vita", &searchBuff) >= 0);
+
+    vita2d_init();
+    vita2d_set_clear_color(RGBA8(0x40, 0x40, 0x40, 0xFF));
+    
+    pgf = vita2d_load_default_pgf();
+
+    SelectScreen();
 
     vita2d_fini();
     vita2d_free_pgf(pgf);
-    sceShellUtilUnlock(SCE_SHELL_UTIL_LOCK_TYPE_PS_BTN);
+    sceShellUtilUnlock(SCE_SHELL_UTIL_LOCK_TYPE_PS_BTN_2);
     sceKernelExitProcess(0);
 }
